30ad23c0161a5da8a2d97ee369d6d724d7c7995e
olegbezpalko74 <sasha_ermakova@ukr.net>
2023-01-19 22:11:48 +0200

    up js

--- js/script.js
+++ js/script.js
@@ -1,71 +1,14 @@
+const logItems = function (items) {
+    console.log('hello');
 
-
-// for (let i = 1; i <= 12; i += 3) {
-//     console.log(i);
-// }
-// console.log('stop');
-
-// const name = 'Mango';
-// console.log(name.split(''));
-
-//  const message = "JavaScript это интересно";
-//  console.log(message.split(" "));
-
-
-//  const words = ["JavaScript", "это", "интересно"];
-//  console.log(words.join("-"));
-
-//  const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
-//  console.log(clients.slice(2,4));
-
-
-// const scores = [1, 2, 3, 4, 5];
-// const deletedScores = scores.splice(0, 3);
-// console.log(scores);
-// console.log(deletedScores);
-
-// const colors = ["red", "green", "blue"];
-// colors.splice(2, 0, "purple");
-// console.log(colors);
-
-// const languages = ["C", "C++", "Java", "JavaScript"];
-// // languages.splice(1, 1, "payton");
-
-
-// languages.splice(2, 1, "C#", "Swift", "Go");
-// // console.log(languages);
-// console.table(languages);
-// console.log(languages[3]);
-
-// const userName = 'Oleg';
-// const age = 20;
-// const gender = 'male';
-
-
-
-// function sayHello(name, age, gender) {
-    
-//     alert('Hello dear ' + name)
-//     if (age < 18) {
-//         alert('hello')
-      
-//     }
-//     if (gender === 'male') {
-//         alert('Welcome')
-//     }
-
-// }
-
-// sayHello(userName, age, gender);
-
-
-function sayHello(greetings) {
-    alert(greetings);
-
+    let total = 0;
+    for (const item of items) {
+        total += item;
+       
+        
+    }
+    return total;
 }
-sayHello('привіт');
-
-const sayHello2 = function(greetings) {
-    alert(greetings);
-}
-sayHello2('LOOOOOO');
+console.log(logItems([2, 6, 9]));
+console.log(logItems([3, 6, 7, 9, 10]));
+console.log(logItems([5, 6, 7, 8, 9]));
