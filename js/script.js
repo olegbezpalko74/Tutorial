90d1816e2c6f014694d5700fd271d5cdc859ee9e
olegbezpalko74 <sasha_ermakova@ukr.net>
2023-01-30 23:02:59 +0200

    pipipipi

--- js/script.js
+++ js/script.js
@@ -1,145 +1,15 @@
-// const logItems = function (items) {
-//     console.log('hello', items);
-
-//     let total = 0;
-//     for (const item of items) {
-//         total += item;
-       
-        
-//     }
-//     return total;
-// }
-
-// const r1 = logItems([2, 6, 9, 125, 145, 197]);
-// console.log(`Загальна сумма покупок ${r1}`);
-
-// // console.log(logItems([2, 6, 9]));
-// console.log(logItems([3, 6, 7, 9, 10]));
-// console.log(logItems([5, 6, 7, 8, 9]));
-
-
-// const logins = ['ololol', 'sososos', 'popopo', 'hohohoh'];
-
-// const findLogin = function (allLogins, loginToFind) {
-//     for (const login of logins) {
-//         if (login === loginToFind) {
-//         return `користувач ${loginToFind} знайдено`;
-//         }    
-//     }
-//     return `користувач ${loginToFind} не знайдено`;   
-// }
-
-// console.log(findLogin(logins, 'ololol'));
-// console.log(findLogin(logins, 'sososos'));
-// console.log(findLogin(logins, 'popopo'));
-// console.log(findLogin(logins, 'hohohoh'));
-
-
-// const playlist = {
-//     name: "Мій супер плейлист",
-//     rating: 5,
-//     tracks: ["track 1","track 2","track 3"],
-//     trackCount: 3,
-//     getName() {
-//         console.log( "hello,my darling",);
-//     },
-   
-// } ;
-// console.log(playlist);
-//  playlist.getName();
-
-const feedback = {
-    good: 6,
-    neitral: 10,
-    bad: 3,
-};
-
-let totalFeedBack = 0;
-
-// const keys = Object.keys(feedback);
-// console.log(keys);
-
-// for (const key of keys) {
-//     console.log(key);
-//     console.log(feedback[key]);
-
-//     totalFeedBack += feedback[key];
-
-// }
-// console.log('total feedback:', totalFeedBack);
-
-// const values = Object.values(feedback);
-// console.log(values);
-
-// for (const value of values){
-//     console.log(value);
-//     totalFeedBack += value;
-// }
-
-// console.log('totalfeedback' , totalFeedBack);
-
-
-// Массив обьектов 
-
-// const freinds = [
-//     {name:'Mango', online: true },
-//     {name:'kiwi', online: false},
-//     {name: 'poly', online: true},
-//     {name:'ajax', online:false},
-// ];
-// console.table(freinds);
-
-// for (const freind of freinds){
-//     console.log(freind);
-
-//     freind.newprop = 555;
-// };
-// console.table(freinds);
-
-// const findFreindByName = function(allFreinds, name){
-//     for (const freind of allFreinds){
-//         // console.log(freind);
-//         // console.log(freind.name);
-//         if (freind.name === name){
-//             return 'vivat'
-//         }
-//     }
-//     return 'not vivat'
-
-// };
-// //  console.log(findFreindByName(freinds, 'poly'));
-//  console.log(findFreindByName(freinds, 'chelsy'));
-
-
-// console.log(..."dfdfdfdf");
-
-// const authors = {
-//     kiwi: 5,
-//     poly: 7,
-//     mango: 4,
-//     ajax: 2,
-// };
-
-// const entries = Object.entries(authors);
-
-// console.log(entries);
-
-// for (const entry of entries){
-//     const [name, rating] = entry;
-//     console.log(name, rating);
-
-// }
-function countTotalSalary(salaries) {
-    let totalSalary = 0;
-    // Change code below this line
-    const name = Object.keys(salaries);
-    const salary = Object.values(salaries);
-    concole.log(salary);
-    
-  
-    // Change code above this line
-    return totalSalary;
+// Колбэк-функция
+function greet(name) {
+    console.log(`Добро пожаловать ${name}.`);
   }
+  
+  // Функция высшего порядка
+  function registerGuest(name, callback) {
+    console.log(`Регистрируем гостя ${name}.`);
+    callback(name);
+  }
+  
+ registerGuest("Манго", greet);
 
 
 
