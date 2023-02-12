9eddd56c3a3d157ecd65d13a5ebce19867063f61
olegbezpalko74 <sasha_ermakova@ukr.net>
2023-02-12 23:35:12 +0200

    popopo

--- js/script.js
+++ js/script.js
@@ -1,61 +1,31 @@
-// // Колбэк-функция
-// function greet(name) {
-//     console.log(`Добро пожаловать ${name}.`);
-//   }
-  
-//   // Функция высшего порядка
-//   function registerGuest(name, callback) {
-//     console.log(`Регистрируем гостя ${name}.`);
-//     callback(name);
-// //   }
-  
-// //  registerGuest("Манго", greet);
+// let count = 10;
 
-//  function registerGuest(name, callback) {
-//   console.log(`Регистрируем гостя ${name}.`);
-//   callback(name);
+ 
+
+//  function sayHello (firstName) {
+//    count = 30;
+//   console.log(count);
+
+
+//    console.log(firstName);
+
+//  }
+//  sayHello('Oleg')
+
+// console.log(count);
+
+// let test = 'oleg';
+
+// if (5 < 10) {
+//   let test = 'correct'
+
+//   console.log(test);
 // }
+// console.log(test);
+// let i = 100;
 
-// // Передаём инлайн функцию greet как колбэк
-// registerGuest("Манго", function greet(name) {
-//   console.log(`Добро пожаловать ${name}.`);
-// });
+// for (let i = 0; i < 10; i++){
+//   console.log(i)
+// }
+// console.log(i);
 
-// // Передаём инлайн функцию notify как колбэк
-// registerGuest("Поли", function notify(name) {
-//   console.log(`Уважаемый(ая) ${name}, ваш номер будет готов через 30 минут.`);
-// });
-
-
-// const numbers = [5, 10, 15, 20, 25];
-
-// // Объявление функции
-// // numbers.forEach(function (number, index) {
-// //   console.log(`Индекс ${index}, значение ${number}`);
-// // });
-// numbers.forEach((number, index) => {
-//   console.log(`Индекс ${index}, значение ${number}`);
-// // });
-
-// const numbers = [5, 10, 15, 20, 25];
-
-// const logMessage = (number, index) => {
-//   console.log(`Индекс ${index}, значение ${number}`);
-// };
-
-// numbers.forEach(logMessage);
-const students = [
-  { name: "Манго", score: 83 },
-  { name: "Поли", score: 59 },
-  { name: "Аякс", score: 37 },
-  { name: "Киви", score: 94 },
-];
-
-// const inAscendingScoreOrder = students.sort(
-//   (firstStudent, secondStudent) => firstStudent.score - secondStudent.score
-// );
-
-const inDescendingScoreOrder = students.sort(
-  (firstStudent, secondStudent) => secondStudent.score - firstStudent.score
-);
-console.log(students)
