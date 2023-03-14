068c01bb8ff92cb4724e0e07c4d559005447989b
olegbezpalko74 <sasha_ermakova@ukr.net>
2023-03-14 13:49:34 +0200

    hh

--- js/script.js
+++ js/script.js
@@ -1,44 +1,5 @@
-
-
-const user = [
-    {
-        name: 'Oleg',
-        age: 48,
-        sex: 'male',
-
-},
-{
-    name: 'Mariy',
-    age: 8,
-    sex: 'female',
-},
-{
-    name: 'Dasha',
-    age: 8,
-    sex: 'female',
-},
-{
-    name: 'Sasha',
-    age: 36,
-    sex: 'female',
-},
-{
-    name: 'Nikoly',
-    age: 71,
-    sex: 'male',
-},
-];
-
-
-user.push({
-    name: 'Vira',
-    age: 71,
-    sex: 'female',
-},)
-
-
-const foundObj = user.filter( (item) => item.age < 17);
-console.log(foundObj);
+console.log('hello');
+console.log( 10 + 50);
 
 
 
