5689b1625b4e19d90a15078302191cf8079d88f6
olegbezpalko74 <sasha_ermakova@ukr.net>
2023-03-31 23:29:41 +0300

    try..catch

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
- 
+  try {
+    console.log('внутри try до myVar');
+    myVar;
+     console.log('внутри try после myVar');
+
+ } catch ( error) {
+    console.log('Ошибка!');
+
+ }
+ console.log('после try...catch');
 
 
 
