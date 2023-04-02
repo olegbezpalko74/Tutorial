77aea2469b926b893c6fc71a33bec5d8e4c8a048
olegbezpalko74 <sasha_ermakova@ukr.net>
2023-04-03 00:04:16 +0300

    111

--- js/script.js
+++ js/script.js
@@ -1,23 +1,8 @@
-const validJSON = '{ "name": "Mango", "age": 3 }';
-const invalidJSON = '{ бекенд плужит } ';
-
-try {
-  console.log(1);
-console.log(JSON.parse(invalidJSON));
-
-console.log(2);
-  
-} catch (error) {
-  console.log( error);
-  if (error.name === 'SyntaxError'); {
-    console.log('help');
-  }
-
-}
 
 
-console.log(999);
 
+
+const BASE_URL = 'https://jsonplaceholder.typicode.com';
 
 
 
