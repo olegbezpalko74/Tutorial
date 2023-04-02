8f17bc7d18ecf0bd8a0e0557bba641277ac7892e
olegbezpalko74 <sasha_ermakova@ukr.net>
2023-04-03 00:04:27 +0300

    11

--- js/script.js
+++ js/script.js
@@ -1,8 +1,34 @@
 
-
-
-
 const BASE_URL = 'https://jsonplaceholder.typicode.com';
+
+
+// const fetchUsers = () =>
+// fetch(`${BASE_URL}/users`).then(res => {
+//   if (!res.ok) {
+//     throw new Error(res.status);
+//   }
+//   return res.json();
+// });
+// fetchUsers();
+
+const fetchUsers = async () => {
+  try {
+    console.log(fetch(`${BASE_URL}/users`)) ;
+    // .then(res => {
+    //   if (!res.ok) {
+    //     throw new Error(res.status);
+    //   }
+    //   return res.json();
+    // });
+    
+  } catch (err) {}
+};
+
+fetchUsers();
+
+
+
+
 
 
 
